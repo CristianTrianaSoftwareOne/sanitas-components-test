ca9188a0dda13e15f72fd6d2c072e05bcc417fd6
cristian triana <cristian.triana@softwareone.com>
2024-01-05 12:07:06 -0500

    base implementation

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { SanInputModule } from '@sanitas-common-components/components';
 
 
 @NgModule({
@@ -12,7 +13,8 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    SanInputModule
   ],
   declarations: [HomePage]
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ISanInputComponent, InputType, LabelPositions } from '@sanitas-common-components/components';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  public roundBorderInputConfig: ISanInputComponent = {} as ISanInputComponent;
+
+  ngOnInit(): void {
+    this.roundBorderInputConfig = {
+      placeHolder: 'Input con Round border with endIcon',
+      type: InputType.TEXT,
+      qa: 'qa-bank',
+      labelText: 'Input con bordes redondos',
+      labelPosition: LabelPositions.FLOATING,
+      required: false,
+      helpertext: 'Input con bordes redondos'
+    };
+  }
 
 }
